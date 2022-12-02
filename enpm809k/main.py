3a48de35c3baef7921989609077181ec154bebef
Jared Dobry <jareddobry10@gmail.com>
2022-12-02 18:22:58 -0500

    Download script

--- enpm809k/main.py
+++ enpm809k/main.py
@@ -2,13 +2,43 @@ from pathlib import Path
 import click
 from json import loads
 import numpy as np
+from glob import glob
+from wget import download
+import tarfile
 
 ALLOW_ALL = True
 ALLOWED_CATEGORIES = ["bicycling", "running", "sports", "transportation", "walking"]
+IMAGE_URL = (
+    "https://datasets.d2.mpi-inf.mpg.de/andriluka14cvpr/mpii_human_pose_v1.tar.gz"
+)
+
+
+def get_dataset():
+    # Check if we already have the dataset downloaded
+    if len(glob("./pose_data/images/*.jpg")) == 0:
+        print("Dataset not found locally, pulling it...")
+        pose_data = Path(__file__).parent.parent / "pose_data"
+        images = pose_data / "images"
+        if not pose_data.exists():
+            pose_data.mkdir()
+            if not images.exists():
+                images.mkdir()
+        temp_path = (
+            Path(
+                __file__,
+            ).parent.parent
+            / "temp.tar.gz"
+        ).as_posix()
+        download(IMAGE_URL, temp_path)
+        tar = tarfile.open(temp_path)
+        tar.extractall(images)
 
 
 @click.command
 def main():
+    # Ensure dataset is available
+    get_dataset()
+
     p = (Path(__file__) / "../../pose_data/annotations.json").resolve()
 
     # Load the annotation data
